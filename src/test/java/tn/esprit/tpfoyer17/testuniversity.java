c2fa516632f7d44bb61d5daa760416059313a4ba
NIDHAL WESLATI <115343876+nidhal333@users.noreply.github.com>
2024-10-28 01:42:06 +0100

    Update testuniversity.java

--- src/test/java/tn/esprit/tpfoyer17/testuniversity.java
+++ src/test/java/tn/esprit/tpfoyer17/testuniversity.java
@@ -1,1 +1,14 @@
+package tn.esprit.tpfoyer17;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@TestMethodOrde(MethodeOrderer.OrderAnnotation.class)
+class testuniversity {
+ @Test
+void contextLoads() {
+ }
+
+}
