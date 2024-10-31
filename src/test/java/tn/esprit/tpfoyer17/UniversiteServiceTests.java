ddd322463bd3f6362e44a460206e1f15bd3954ce
NIDHAL WESLATI <115343876+nidhal333@users.noreply.github.com>
2024-10-31 20:14:17 +0100

    Update UniversiteServiceTests.java

--- src/test/java/tn/esprit/tpfoyer17/UniversiteServiceTests.java
+++ src/test/java/tn/esprit/tpfoyer17/UniversiteServiceTests.java
@@ -2,29 +2,30 @@ package tn.esprit.tpfoyer17;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import tn.esprit.tpfoyer17.entities.Universite;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tn.esprit.tpfoyer17.repositories.UniversiteRepository;
+import tn.esprit.tpfoyer17.entities.Universite;
 import tn.esprit.tpfoyer17.services.impementations.UniversiteService;
 
-@ExtendWith(MockitoExtension.class)
-@SpringBootTest
 
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
 @Slf4j
-public class UniversiteServiceTests {
+class UniversiteServiceTests {
     @Autowired
     UniversiteService universiteService;
-    UniversiteRepository universiteRepository;
     @Test
     void addUniversitytest() {
         Universite newuniversitie= Universite.builder().nomUniversite("ESPRIT").adresse("ariena")
                 .build();
-        Mockito.when(universiteRepository.save(Mockito.any(Universite.class))).thenReturn(newuniversitie);
         Universite saveUniversity=universiteService.addUniversity(newuniversitie);
+        Assertions.assertNotNull(saveUniversity.getIdUniversite());
     }
 }
