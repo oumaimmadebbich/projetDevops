9dc194c41323391bf629306745ad5454ec3db384
NIDHAL WESLATI <115343876+nidhal333@users.noreply.github.com>
2024-10-31 21:02:44 +0100

    Update UniversiteServiceTests.java

--- src/test/java/tn/esprit/tpfoyer17/UniversiteServiceTests.java
+++ src/test/java/tn/esprit/tpfoyer17/UniversiteServiceTests.java
@@ -1,7 +1,7 @@
 package tn.esprit.tpfoyer17;
 
 import lombok.extern.slf4j.Slf4j;
-import static org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.mockito.Mockito.*;
