98eae924c67179121500f8319ed6320728bfd875
NIDHAL WESLATI <115343876+nidhal333@users.noreply.github.com>
2024-10-31 20:37:27 +0100

    Update UniversiteServiceTests.java

--- src/test/java/tn/esprit/tpfoyer17/UniversiteServiceTests.java
+++ src/test/java/tn/esprit/tpfoyer17/UniversiteServiceTests.java
@@ -2,30 +2,32 @@ package tn.esprit.tpfoyer17;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import tn.esprit.tpfoyer17.repositories.UniversiteRepository;
 import tn.esprit.tpfoyer17.entities.Universite;
+import tn.esprit.tpfoyer17.repositories.UniversiteRepository;
 import tn.esprit.tpfoyer17.services.impementations.UniversiteService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import java.util.*;
 
+@ExtendWith(MockitoExtension.class)
 
-@ExtendWith(SpringExtension.class)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@SpringBootTest
-@Slf4j
-class UniversiteServiceTests {
-    @Autowired
+public class UniversiteServiceTestsMOck {
+    @Mock
     UniversiteService universiteService;
+     @Mock
+    UniversiteRepository universiteRepository;
     @Test
     void addUniversitytest() {
-        Universite newuniversitie= Universite.builder().nomUniversite("ESPRIT").adresse("ariena")
-                .build();
+        Universite newuniversitie= new Contrat();
+        when(universiteRepository.save(newuniversitie)).thenReturn(newuniversitie);
         Universite saveUniversity=universiteService.addUniversity(newuniversitie);
-        Assertions.assertNotNull(saveUniversity.getIdUniversite());
+        assertEquals(newuniversiti,saveUniversity);
+        verify(universiteRepository, times(1)).save(newuniversitie);
     }
 }
